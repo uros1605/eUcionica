67856722561c9446fe973006494b0ea7b93e36c6
uros1605 <uros.krstic5000@gmail.com>
2024-04-03 23:12:05 +0200

    Upload

---
+++ DBContext/DBContextClass.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EntityLib;
+using System.Reflection.Emit;
+
+namespace DBContext
+{
+    public class DBContextClass : DbContext
+    {
+
+        public DbSet<Predmet> Predmet { get; set; }
+        public DbSet<Nivo> Nivo { get; set; }
+        public DbSet<Oblast> Oblast { get; set; }
+        public DbSet<Pitanje> Pitanje { get; set; }
+
+
+        public DBContextClass(DbContextOptions<DBContextClass> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Predmet>()
+                .Property(p => p.Naziv)
+                .IsRequired();
+            modelBuilder.Entity<Pitanje>()
+                .Property(p => p.Tekst)
+                .IsRequired();
+            modelBuilder.Entity<Pitanje>()
+                .Property(p => p.Odgovor)
+                .IsRequired();
+            modelBuilder.Entity<Pitanje>()
+             .Property(p => p.NivoID)
+             .IsRequired();
+            modelBuilder.Entity<Pitanje>()
+              .Property(p => p.OblastID)
+              .IsRequired();
+            modelBuilder.Entity<Pitanje>()
+               .Property(p => p.PredmetID)
+               .IsRequired();
+
+
+            modelBuilder.Entity<Oblast>()
+               .Property(o => o.Naziv)
+               .IsRequired();
+            modelBuilder.Entity<Oblast>()
+               .Property(o => o.PredmetID)
+               .IsRequired();
+
+            modelBuilder.Entity<Nivo>()
+               .Property(n => n.Naziv)
+               .IsRequired();
+            
+
+
+            modelBuilder.Entity<Predmet>()
+                .HasMany(s => s.PitanjazaPredmet).WithOne(p => p.Predmet).HasForeignKey(p => p.PredmetID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Predmet>()
+                .HasMany(s => s.OblastzaPredmet).WithOne(p => p.Predmet).HasForeignKey(p => p.PredmetID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Oblast>()
+                .HasMany(s => s.OblastzaPitanje).WithOne(p => p.Oblast).HasForeignKey(p => p.OblastID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Nivo>()
+                .HasMany(s => s.NivozaPitanje).WithOne(p => p.Nivo).HasForeignKey(p => p.NivoID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
---
+++ EntityLib/Oblast.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLib
+{
+    public class Oblast
+    {
+        public Predmet Predmet { get; set; }
+        public int PredmetID { get; set; }
+        [ForeignKey("PredmetID")]
+
+        [Key]
+        public int OblastID { get; set; }
+        public string Naziv { get; set; }
+
+        public ICollection<Pitanje> OblastzaPitanje { get; set; } = new List<Pitanje>();
+    }
+}
